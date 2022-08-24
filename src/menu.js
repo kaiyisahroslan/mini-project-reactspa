bde46ec323c0e91d35324a6489a21d69c0bfd3ba
kaiyisahroslan <91020095+kaiyisahroslan@users.noreply.github.com>
2022-08-24 16:00:21 +0800

    Add files via upload

---
+++ src/menu.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Menu() {
+    return (
+        // SPA routes
+        <div>
+            <h1>Menu component</h1>
+            <Link to="/">Home</Link> &nbsp;&nbsp;
+            <Link to="/friends">Friends</Link> &nbsp;&nbsp;
+            <Link to="/help">Help</Link> &nbsp;&nbsp;
+        </div>
+    );
+}
+
+export default Menu;
